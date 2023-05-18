f5d8544adc32ed16385797688db1123b0a02d2b0
Arpit Mohapatra <arpitmohapatra18@gmail.com>
2023-05-18 02:36:24 -0400

    Update script.js

--- script.js
+++ script.js
@@ -18,27 +18,29 @@ window.onload = () => {
     });
   });
   
+  // Handle the install button click event
+  
+  
   var isIOS = /iPad|iPhone|iPod/.test(navigator.userAgent);
   
   if (isIOS && window.navigator.standalone === false) {
     // Show the install button
     var installButton = document.getElementById('install-button');
     installButton.style.display = 'block';
-    installButton.addEventListener('click', addToHomeScreen);
+    installButton.addEventListener('click', () => {
+      if (isIOS && window.navigator.standalone === false) {
+        var userAgent = window.navigator.userAgent.toLowerCase();
+        if (userAgent.indexOf('safari') !== -1 && userAgent.indexOf('chrome') === -1) {
+          // Safari on iOS
+          var instructions = 'To add this web app to your home screen, tap the share button and select "Add to Home Screen".';
+          alert(instructions);
+        }
+      }
+    });
   }
 };
 
-// Handle the install button click event
-function addToHomeScreen() {
-  if (isIOS && window.navigator.standalone === false) {
-    var userAgent = window.navigator.userAgent.toLowerCase();
-    if (userAgent.indexOf('safari') !== -1 && userAgent.indexOf('chrome') === -1) {
-      // Safari on iOS
-      var instructions = 'To add this web app to your home screen, tap the share button and select "Add to Home Screen".';
-      alert(instructions);
-    }
-  }
-}
+
 
 function CountdownTracker(label, value) {
   var el = document.createElement("span");
